2da2576ae89f64093ba59b8ed795c3f56323d491
Pbcd02 <paul.bcd.2002@gmail.com>
2024-04-24 23:18:46 -0400

    Subiendo

--- Practica_FSM_Elevador/Practica_FSM_Elevador.ino
+++ Practica_FSM_Elevador/Practica_FSM_Elevador.ino
@@ -2,6 +2,7 @@
 #define LED2 4
 #define LED3 5
 #define ESPERA 10
+#define DELAYELEVADOR 2000
 
 int LLAMADO = 0;
 char dato;
@@ -18,6 +19,9 @@ typedef enum
 void setup() 
 {
   Serial.begin(9600);
+  pinMode(LED1,OUTPUT);
+  pinMode(LED2,OUTPUT);
+  pinMode(LED3,OUTPUT);
 }
 
 void loop() 
@@ -26,7 +30,7 @@ void loop()
   {
     dato=Serial.read();
   }
-  switch (estado) 
+  switch(estado)
   {
     case Piso1:
       digitalWrite(LED1, HIGH);
@@ -34,11 +38,15 @@ void loop()
       digitalWrite(LED3, LOW);
       if(dato=='1') 
       {
+        Serial.println("Subiendo...");
+        delay(DELAYELEVADOR);
         estado = Piso2;
         Serial.println("Haz llegado al piso 2");
       }
       else if(dato=='2')
       {
+        Serial.println("Subiendo...");
+        delay(DELAYELEVADOR);
         estado = Piso3;
         Serial.println("Haz llegado al piso 3");
       }
@@ -50,11 +58,15 @@ void loop()
       digitalWrite(LED3, LOW);
       if(dato=='0') 
       {
+        Serial.println("Bajando...");
+        delay(DELAYELEVADOR);
         estado = Piso1;
         Serial.println("Haz llegado al piso 1");
       }
       else if(dato=='2')
       {
+        Serial.println("Subiendo...");
+        delay(DELAYELEVADOR);
         estado = Piso3;
         Serial.println("Haz llegado al piso 3");
       }
@@ -66,11 +78,15 @@ void loop()
       digitalWrite(LED3, HIGH);
       if(dato=='0') 
       {
+        Serial.println("Bajando...");
+        delay(DELAYELEVADOR);
         estado = Piso1;
         Serial.println("Haz llegado al piso 1");
       }
       else if(dato=='1')
       {
+        Serial.println("Bajando...");
+        delay(DELAYELEVADOR);
         estado = Piso2;
         Serial.println("Haz llegado al piso 2");
       }
