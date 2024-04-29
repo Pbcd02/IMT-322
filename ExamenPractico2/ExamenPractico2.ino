82004660a07f9cde8f9e5fc2db5c510419e1b1eb
Pbcd02 <paul.bcd.2002@gmail.com>
2024-04-28 22:37:38 -0400

    ActualizandoYMejorando

--- ExamenPractico2/ExamenPractico2.ino
+++ ExamenPractico2/ExamenPractico2.ino
@@ -3,68 +3,143 @@
 #define TB2 5000
 #define TB3 7000
 #define BAUDRATE 9600
-bool T=true;
-char dato;
-char compra;
+#define PULSADOR 3
+//Definimos las variables
+int dato=0;
+int compra=0;
+int precioBebida;
+int vuelto;
+bool T=false;
+bool S=false;
+int contador = 0;
+
 typedef enum
 {
+  Inicio,
   Compra,
   Bebida1,
   Bebida2,
-  Bebida3
-}Maquina;
-  Maquina estado = Compra;
+  Bebida3,
+  Reinicio
+} Maquina;
+
+Maquina estado = Inicio;
+
 void setup() 
 {
   Serial.begin(BAUDRATE);
-  Serial.println("ingrese un monto mayor a 2$");
+  pinMode(PULSADOR, INPUT_PULLUP);
 }
 
 void loop() 
 {
-  if(Serial.available()>0)
+  switch (estado)
   {
-    dato=Serial.read();
-    compra=Serial.read();
-  }
-    if(dato=='1')
+  case Inicio:
+  Serial.println("Ingrese un monto mayor a $2");
+    while (!Serial.available()||dato==0)
+    {
+      dato = Serial.parseInt();
+      T=true;
+    }
+    
+    Serial.print("El monto de dinero ingresado fue: ");
+    Serial.println(dato);
+    
+    if (dato < 2) 
     {
       Serial.println("Monto Insuficiente, no se puede introducir a la maquina");
     }
-    else if(dato=='2'||dato=='5'||dato=='7'||T==false)
+    Serial.println("Bienvenido, ¿qué bebida deseas?");
+    Serial.println("500ml cuesta $2 (1), 1000ml cuesta $5 (2), 1500ml cuesta $7 (3)");  
+    
+    while (T==true)
     {
-      switch(estado)
+      if(Serial.available() > 1)
       {
-        case Compra:
-          Serial.println("Bienvenido que bebida deseas");
-          Serial.println("500ml cuesta 2$(B),1000ml cuesta 5$(N), 1500ml cuesta 7$(M)");
+      S=true;
+        if(compra==0)
+        {
+          compra = Serial.parseInt();
+        }
+        if(compra==1||compra==2||compra==3)
+        {
+          estado=Compra;
           T=false;
-      if(compra=='B')
-        {
-          estado=Bebida1;
         }
-      else if(compra=='N')
-        {
-          estado=Bebida2;
-        }
-        else if(compra=='M')
-        {
-          compra=Bebida3;
-        }
-        break;
-        case Bebida1:
-            Serial.println("Haz Elegido la bebida de 500ml");
-            mostrarBarraDeCarga(TB1);
-        break;
-        case Bebida2:
-            Serial.println("Haz Elegido la bebida de 1000ml");
-            mostrarBarraDeCarga(TB2);
-        break;
-
-        case Bebida3:
-            Serial.println("Haz Elegido la bebida de 1500ml");
-            mostrarBarraDeCarga(TB3);
-        break;
       }
     }
+  break;
+
+    case Compra:
+    if(compra==1)
+    {
+      estado = Bebida1;
+    }
+    if(compra==2)
+    {
+      estado = Bebida2;
+    }
+    if(compra==3)
+    {
+      estado = Bebida3;
+    }
+    break;
+
+    case Bebida1:
+      precioBebida = 2;
+      Serial.println("La bebida elegida fue 500ml");
+      cambio(TB1);
+    break;
+
+    case Bebida2:
+      precioBebida = 5;
+      Serial.println("La bebida elegida fue 1000ml");
+      cambio(TB2);
+    break;
+
+    case Bebida3:
+      precioBebida = 7;
+      Serial.println("La bebida elegida fue 1500ml");
+      cambio(TB3);
+    break;
+      
+    case Reinicio:
+    Serial.println("Oprime el boton para el reinicio del sistema");
+    Boton();
+    break;
+
+    default:
+      Serial.println("Bebida no válida. Reiniciando el ciclo.");
+      return;
+  }
+}
+void cambio(int n) 
+{
+  vuelto = dato - precioBebida;
+  mostrarBarraDeCarga(n);
+  if (vuelto >= 0) 
+  {
+    Serial.print("Su vuelto es de $");
+    Serial.println(vuelto);
+  }
+  else if (vuelto < 0) 
+  {
+    Serial.println("Error: monto insuficiente para comprar la bebida.");
+    dato=0;
+    compra=0;
+    estado=Inicio;
+  }
+  estado = Reinicio;
+}
+void Boton()
+{
+  delay(2000);
+  if (digitalRead(PULSADOR) == LOW) 
+  {
+    dato=0;
+    compra=0;
+    Serial.println("Botón presionado. Reiniciando el sistema");
+    estado=Inicio;
+  }
 }
